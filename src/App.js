4b23152114a233738d333f247012db3dc96af3e4
LordArshen <m.r.barrera@hotmail.com>
2022-04-18 19:07:09 -0500

    proof of dark mode- content

--- src/App.js
+++ src/App.js
@@ -5,16 +5,37 @@ import Content from "./components/Content";
 import { Route, Routes } from "react-router-dom";
 import PersistentDrawerLeft from "./components/PersistentDrawerLeft";
 
+
+import { createTheme, ThemeProvider } from "@material-ui/core/styles";
+import  { useState } from "react";
+
+
+
+
+
 const App = () => {
+
+    const [toggleDark, settoggleDark] = useState(false);
+    const myTheme = createTheme({
+      
+      // Theme settings
+      palette: {
+        type: toggleDark ? "dark" : "light",
+      },
+    });
+
     return (
+    <ThemeProvider theme={myTheme}>
         <div className="App">
             <PersistentDrawerLeft />
             <Routes>
-                <Route path="/" element={<Content />} />
+                <Route path="/" element={<Content toggleDark={toggleDark}
+        settoggleDark={settoggleDark} />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
             </Routes>
         </div>
+    </ThemeProvider>
     );
 };
 
