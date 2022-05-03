cac368fc89b6a7ba88075318a849e519c9091098
LordArshen <m.r.barrera@hotmail.com>
2022-05-02 19:05:44 -0500

    login app

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter } from "react-router-dom";
 import Amplify from 'aws-amplify';
-import config from '.aws-exports';
+import config from './aws-exports';
 
 
 Amplify.configure(config);
